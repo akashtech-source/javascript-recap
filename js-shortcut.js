5cd8df80da18dab32a630363304e09e18d324265
akashtech-source <mr.ifranakash@gmail.com>
2021-02-16 23:37:36 +0600

    part 4

---
+++ js-shortcut.js
@@ -0,0 +1,34 @@
+// const money = 150;
+// const active = true;
+// let food;
+// if (money > 100) {
+//     food = 'biriyani';
+// }
+// else{
+//     food = 'alu vorta';
+// }
+// // console.log(food);
+
+// // // উপরের জিনিসটাই এখানে শর্টকাটে করা হলো Ternary করে। শর্টকাটে  If Else  লেখার সিস্টেম কে  Ternary বলে।
+
+// let food = money > 100 ? 'biriyani' : 'alu vorta'
+
+// const cssClass = active ? 'active' : 'inactive'
+// // console.log(cssClass);
+// // function call shortcut alterrnative
+// active ? displayUser() : hideUser()
+
+// // 
+// active && displayUser() 
+
+// // 
+// const x = active && 5;
+// console.log(x);
+// // মোস্ট ইম্পর্ট্যান্ট হলো, কোনো একটা নাম্বার যদি স্ট্রিং এ থাকে তাহলে সেই স্ট্রিং এর আগে প্লাস সাইন দিলে সেই নাম্বার স্ট্রিং টা পুরোপুরি নাম্বারে কনভার্ট হয়ে যাবে।
+//  const number = +'45';
+//  console.log(number);
+
+//  নাম্বার কে স্ট্রিং এ রুপান্তর
+const numText = 66 + '';
+console.log(numText);
+
